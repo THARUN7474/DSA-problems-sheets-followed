bc7388744d7e1a15a60f8c032c8cc85a21833f26
THARUN7474 <bandatharun74@gmail.com>
2025-01-05 20:53:25 +0530

    gfg

---
+++ GFG_160/10_maxm_sum_array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr)
+    {
+        // code here...
+        int res = arr[0];
+        int maxsum = arr[0];
+
+        for (int i = 1; i < arr.size(); i++)
+        {
+            maxsum = max(maxsum + arr[i], arr[i]);
+            res = max(res, maxsum);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
